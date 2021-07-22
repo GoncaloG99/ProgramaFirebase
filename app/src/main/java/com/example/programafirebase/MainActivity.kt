681e09663b0f06aeb9e600351338d088be38af6e
GoncaloG <guimaraes1920@hotmail.com>
2021-07-22 02:40:24 +0100

    Setup

--- app/src/main/java/com/example/programafirebase/MainActivity.kt
+++ app/src/main/java/com/example/programafirebase/MainActivity.kt
@@ -11,25 +11,25 @@ import com.example.programafirebase.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
 
-        val navView: BottomNavigationView = binding.navView
+        setContentView(R.layout.activity_main)
 
-        val navController = findNavController(R.id.nav_host_fragment_activity_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
-            )
-        )
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
+        findViewById<BottomNavigationView>(R.id.nav_view).setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.navigation_people -> {
+                    //TODO: Show people fragment
+                    true
+                }
+                R.id.navigation_myAccount -> {
+                    //TODO: Show my account fragment
+                    true
+                }
+                else -> false
+            }
+        }
     }
 }
